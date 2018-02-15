94fc087acb4415de5645f2ec3cef97b9e4e81597
corente94 <corente94@hotmail.fr>
2018-02-15 15:54:30 +0100

    Maj
    AJout de particule + follow camera + pieces pour se deplacer

--- Test piece 3D/Assets/Deplacement.cs
+++ Test piece 3D/Assets/Deplacement.cs
@@ -2,8 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Deplacement : MonoBehaviour {
+public class Deplacement : MonoBehaviour
+{
 
+	public Rigidbody rb;
+
+	public float avant = 50f;
+
+	public float arriere = -50f;
+
+	public float gauche = -50f;
+
+	public float droite = 50f;
+	
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +22,24 @@ public class Deplacement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (Input.GetKey("z"))
+		{
+			rb.AddForce(avant*Time.deltaTime,0,0);
+		}
+		if (Input.GetKey("q"))
+		{
+			rb.AddForce(0,0,gauche*Time.deltaTime);
+		}
+		if (Input.GetKey("d"))
+		{
+			rb.AddForce(0,0,droite*Time.deltaTime);
+		}
+		if (Input.GetKey("s"))
+		{
+			rb.AddForce(arriere*Time.deltaTime,0,0);
+		}
+		
 		
 	}
 }
