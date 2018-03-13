327c790b11c51818c4a6ff179f12fb3daffbe355
Marine Charra <marine.charra@github.com>
2018-03-13 19:26:04 +0100

    Maj Mapping
    Trou dans les murs et lave

---
+++ Map/Assets/Scripts/ListExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ListExtensions
+
+{
+    public static IList<T> Shuffle<T>(this IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = UnityEngine.Random.Range(0, n - 1);
+            T tmp = list[k];
+            list[k] = list[n];
+            list[n] = list[k];
+
+        }
+
+        return list;
+    }
+}
