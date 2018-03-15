825f3a24922426d15396e24aeb2ad20de06e938a
corente94 <corente94@hotmail.fr>
2018-03-15 16:18:29 +0100

    Ajout du projet unity

---
+++ Projet/Assets/MovePlayerSmooth.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayerSmooth : MonoBehaviour {
+
+	public bool hasToMoveU;
+	public bool hasToMoveD;
+	public bool hasToMoveL;
+	public bool hasToMoveR;
+
+	public Vector3 initialPosition;
+	// Use this for initialization
+	void Start () {
+		hasToMoveU = false;
+		hasToMoveD = false;
+		hasToMoveL = false;
+		hasToMoveR = false;
+		initialPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.UpArrow) || hasToMoveU)
+		{
+			transform.Translate(0, 0, 0.1f);
+			hasToMoveU = transform.position.z < (initialPosition + new Vector3(0, 0, 10)).z;
+			if (!hasToMoveU)
+			{
+				initialPosition = transform.position;
+			}
+		}
+		else if (Input.GetKeyDown(KeyCode.DownArrow) || hasToMoveD)
+		{
+			if (Input.GetKeyDown(KeyCode.DownArrow))
+			{
+				transform.Rotate(0, 180, 0);
+			}
+			transform.Translate(0, 0, 0.1f);
+			hasToMoveD = transform.position.z > (initialPosition + new Vector3(0, 0, -10)).z;
+			if (!hasToMoveD)
+			{
+				initialPosition = transform.position;
+			}
+		}
+		else if (Input.GetKeyDown(KeyCode.LeftArrow) || hasToMoveL)
+		{
+			if (Input.GetKeyDown(KeyCode.LeftArrow))
+			{
+				transform.Rotate(0, -90, 0);
+			}
+			transform.Translate(0, 0, 0.1f);
+			hasToMoveL = transform.position.x > (initialPosition + new Vector3(-10, 0, 0)).x;
+			if (!hasToMoveL)
+			{
+				initialPosition = transform.position;
+			}
+		}
+		else if (Input.GetKeyDown(KeyCode.RightArrow) || hasToMoveR)
+		{
+			if (Input.GetKeyDown(KeyCode.RightArrow))
+			{
+				transform.Rotate(0, 90, 0, Space.Self);
+			}
+			transform.Translate(0, 0, 0.1f);
+			hasToMoveR = transform.position.x <= (initialPosition + new Vector3(10, 0, 0)).x;
+			if (!hasToMoveR)
+			{
+				initialPosition = transform.position;
+			}
+		}
+	}
+}
---
+++ Projet/Assets/Prefab/Blocs.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using NUnit.Framework.Constraints;
+using UnityEngine;
+
+public class Bloc
+{
+    public bool west, east, south, north, artefact;
+    public bool type1, type2, type3;
+    private int type;
+    public int rotate;
+    public int xpos, zpos;
+    public VisualBloc obj;
+    
+    public Bloc ()
+    {
+        type = Random.Range(1,4);
+        rotate = Random.Range(1,4);
+        //rotate = 1;
+        artefact = Random.Range(0,1) == 1;
+        type1 = false;
+        type2 = false;
+        type3 = false;
+       // obj = new VisualBloc();
+        
+        Typed();
+    }
+
+    private void Typed()
+    {
+        if (type == 1)
+        {
+            type1 = true;
+        }
+        else if (type == 2)
+        {
+            type2 = true;
+        }
+        else if (type == 3)
+        {
+            type3 = true;
+        }
+    }
+    
+    
+}
