2acc19e2b8b4625fffa36dd517de8ef37eda3115
NoraDelfau <35450598+NoraDelfau@users.noreply.github.com>
2018-03-15 15:31:54 +0100

    MAJ

--- New_Kolasi/Assets/MovePlayerSmooth.cs
+++ New_Kolasi/Assets/MovePlayerSmooth.cs
@@ -1,33 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using DefaultNamespace;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class MovePlayer : MonoBehaviour
-{
+public class MovePlayerSmooth : MonoBehaviour {
+
 	public bool hasToMoveU;
 	public bool hasToMoveD;
 	public bool hasToMoveL;
 	public bool hasToMoveR;
 
 	public Vector3 initialPosition;
-
-	public Quaternion initialRotation;
 	// Use this for initialization
-	void Start ()
-	{
+	void Start () {
 		hasToMoveU = false;
 		hasToMoveD = false;
 		hasToMoveL = false;
 		hasToMoveR = false;
 		initialPosition = transform.position;
-		initialRotation = transform.rotation;
 	}
 	
 	// Update is called once per frame
-	void Update()
-	{
+	void Update () {
 		if (Input.GetKeyDown(KeyCode.UpArrow) || hasToMoveU)
 		{
 			transform.Translate(0, 0, 0.1f);
@@ -36,47 +29,45 @@ public class MovePlayer : MonoBehaviour
 			{
 				initialPosition = transform.position;
 			}
-			//transform.Translate(0, 0, 10);
 		}
 		else if (Input.GetKeyDown(KeyCode.DownArrow) || hasToMoveD)
 		{
-			transform.Translate(0, 0, -0.1f);
+			if (Input.GetKeyDown(KeyCode.DownArrow))
+			{
+				transform.Rotate(0, 180, 0);
+			}
+			transform.Translate(0, 0, 0.1f);
 			hasToMoveD = transform.position.z > (initialPosition + new Vector3(0, 0, -10)).z;
 			if (!hasToMoveD)
 			{
 				initialPosition = transform.position;
-				initialRotation = transform.rotation;
 			}
-			/*transform.Rotate(0, 180, 0);
-			transform.Translate(0, 0, 10);*/
 		}
 		else if (Input.GetKeyDown(KeyCode.LeftArrow) || hasToMoveL)
 		{
-			transform.Translate(-0.1f, 0, 0);
+			if (Input.GetKeyDown(KeyCode.LeftArrow))
+			{
+				transform.Rotate(0, -90, 0);
+			}
+			transform.Translate(0, 0, 0.1f);
 			hasToMoveL = transform.position.x > (initialPosition + new Vector3(-10, 0, 0)).x;
 			if (!hasToMoveL)
 			{
 				initialPosition = transform.position;
-				initialRotation = transform.rotation;
 			}
-			/*transform.Rotate(0, -90, 0);
-			transform.Translate(0, 0, 10);*/
 		}
 		else if (Input.GetKeyDown(KeyCode.RightArrow) || hasToMoveR)
 		{
 			if (Input.GetKeyDown(KeyCode.RightArrow))
 			{
-				transform.Rotate(0, 90, 0);
+				transform.Rotate(0, 90, 0, Space.Self);
 			}
 			transform.Translate(0, 0, 0.1f);
-			hasToMoveR = transform.position.x < (initialPosition + new Vector3(10, 0, 0)).x;
+			hasToMoveR = transform.position.x <= (initialPosition + new Vector3(10, 0, 0)).x;
 			if (!hasToMoveR)
 			{
 				initialPosition = transform.position;
-				initialRotation = transform.rotation;
 			}
-			/*transform.Rotate(0, 90, 0);
-			transform.Translate(0, 0, 10);*/
 		}
 	}
 }
