5d72baf29230fd0a373a1bf86297e4c992a58b46
Marine Charra <marine.charra@github.com>
2018-05-02 12:35:01 +0200

    Projet graphique
    Pronjet 23 mars -> nouvelle lave

---
+++ Projet_23_mars copie/Projet/Assets/Map/Scripts/Teleportation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleportation : MonoBehaviour
+{
+	public Transform destination;
+	
+	// Use this for initialization
+	void Start () {
+		
+		destination = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		OnTriggerEnter();
+	}
+	
+	private void OnTriggerEnter() {
+		Debug.Log(this.tag);
+		if (CompareTag("Player"))
+			transform.position = destination.position;
+	}
+}
