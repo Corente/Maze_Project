e51ee80e654d5eb4c65d5bd2f33cc87133f27a47
Corentin <corente94@hotmail.fr>
2018-06-05 17:41:05 +0200

    Changement artefact

--- Projet/Assets/Map/Scripts/Maze.cs
+++ Projet/Assets/Map/Scripts/Maze.cs
@@ -48,11 +48,23 @@ public class Maze : NetworkBehaviour
 
 	private float timer;
 	
+	public GameObject tree;
+	public GameObject statue;
+	public GameObject coin;
+	
+	protected GameObject artefact;
+
+	public bool isTaken = false;
+
+	
 	
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		grid = new Bloc[width,height];
 		Init();
+		
+		artefact = Creation();
 		Postion1.transform.position = grid[0, 0].obj.transform.position + new Vector3(0,3,0);
 		Postion2.transform.position = grid[0, 8].obj.transform.position + new Vector3(0,3,0);
 		Postion3.transform.position = grid[8, 0].obj.transform.position + new Vector3(0,3,0);
@@ -76,6 +88,10 @@ public class Maze : NetworkBehaviour
 		{
 			dico(memoire_ligne,memoire_direction);
 			
+		}
+		if (isTaken)
+		{
+			Destroy(artefact);
 		}
 		
 	}
@@ -604,8 +620,47 @@ public class Maze : NetworkBehaviour
 		}
 	}
 		
+//#######################################################################################################################################################################################
+//                             ARTEFACT
+	private void OnTriggerEnter(Collider other)
+	{
 		
+		Destroy(gameObject);
+		/*if (other.gameObject.CompareTag("Player"))
+		{
+			//Destroy(artefact);
+			//artefact = Creation();
+			isTaken = true;
+		}*/
+	}
+
+	/*
+	 * Creation d'un nouvel artefact
+	 */
+	
+	public GameObject Creation()
+	{
+		GameObject gameObject;
+		int rdn = Random.Range(0, 3);
+		if (rdn == 1)
+		{
+			artefact = tree;
+		}
+		else if (rdn == 2)
+		{
+			artefact = statue;
+		}
+		else
+		{
+			artefact = coin;
+		}
+		float x = Random.Range(0, 9);
+		float z = Random.Range(1, 10);
+		float y = 3;
 		
+		grid[(int) x, (int) z].AMB = Instantiate(artefact, new Vector3((float) (grid[(int) x, (int) z]).xpos * 20, y, (float) (grid[(int) x, (int) z]).zpos * 20), new Quaternion(0, 0, 0, 0));
+		return grid[(int) x, (int) z].AMB;
+	}
 }
 
 
