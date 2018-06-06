dfd6ca40ed8fee2618c8e9f4b181f093a801d874
Corentin <corentin.ourvoy@epita.fr>
2018-06-06 15:44:25 +0200

    Ajout qte & prefab

---
+++ Projet/Assets/QTE/QTE_Intrus.cs
@@ -0,0 +1,133 @@
+﻿ using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class QTE_Intrus : WIN
+{
+
+
+	public Button btn;
+	public Image rules;
+	public Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn10, btn11, btn12;
+	private List<Button> listbtn;
+	private System.Random random;
+
+	// Use this for initialization
+	void Start()
+	{
+		btn = btn.GetComponent<Button>();
+		btn1 = btn1.GetComponent<Button>();
+		btn2 = btn2.GetComponent<Button>();
+		btn3 = btn3.GetComponent<Button>();
+		btn4 = btn4.GetComponent<Button>();
+		btn5 = btn5.GetComponent<Button>();
+		btn6 = btn6.GetComponent<Button>();
+		btn7 = btn7.GetComponent<Button>();
+		btn8 = btn8.GetComponent<Button>();
+		btn9 = btn9.GetComponent<Button>();
+		btn10 = btn10.GetComponent<Button>();
+		btn11 = btn11.GetComponent<Button>();
+		btn12 = btn12.GetComponent<Button>();
+		listbtn = new List<Button>();
+		listbtn.Add(btn);
+		listbtn.Add(btn1);
+		listbtn.Add(btn2);
+		listbtn.Add(btn3);
+		listbtn.Add(btn4);
+		listbtn.Add(btn5);
+		listbtn.Add(btn6);
+		listbtn.Add(btn7);
+		listbtn.Add(btn8);
+		listbtn.Add(btn9);
+		listbtn.Add(btn10);
+		listbtn.Add(btn11);
+		listbtn.Add(btn12);
+		
+		rules = rules.GetComponent<Image>();
+		rules.enabled = false;
+
+		random = new System.Random();
+		begin = true;
+		end = false;
+		
+		timer = 12.0f;
+		X_max = Screen.width - 10;
+		Y_max = Screen.height - 10;
+
+		for (int i = 0; i < listbtn.Count; i++)
+		{
+			listbtn[i].transform.position = new Vector3(-4000, -4000, 0);
+			listbtn[i].onClick.AddListener(enable);
+			if (i != 0)
+			{
+				listbtn[i].onClick.AddListener(lose);
+			}
+			else
+			{
+				listbtn[i].onClick.AddListener(winning);
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		timer -= Time.deltaTime;
+
+		if (enabled && begin && timer <= 7)
+		{
+			foreach (var bouton in listbtn)
+			{
+				randompos(bouton);
+			}
+			rules.enabled = true;
+			begin = false;
+		}
+		if (enabled && !end && timer <= 0)
+		{
+			rules.enabled = false;
+			enable();
+			lose();
+		}
+
+	}
+
+//FONCTIONS
+
+	public void enable()
+	{
+		
+		foreach (var bouton in listbtn)
+		{
+			bouton.enabled = false;
+			bouton.transform.position = new Vector3(-4000, -4000, 0);
+		}
+		end = true;
+		enabled = false;
+	}
+
+	public void lose()
+	{
+		win = false;
+	}
+
+	public void winning()
+	{
+		win = true;
+	}
+
+
+
+	public void randompos(Button btns)
+	{
+		int x = random.Next(10, X_max - 10);
+		int y = random.Next(10, Y_max - 10);
+		btns.transform.position = new Vector3(x, y, 0);
+	}
+
+
+}
