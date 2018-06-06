0da838b22cd0e16f064b2428b55e9312a1efedae
Marine Charra <marine.charra@github.com>
2018-06-06 15:15:54 +0200

    QTE

---
+++ Projet_23_mars/Projet/Assets/QTE/WIN.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class WIN : MonoBehaviour
+{
+
+	protected bool win;
+	protected bool end;
+
+	protected bool begin;
+	protected int X_max;
+	protected int Y_max;
+	protected float timer;
+	
+	
+	public bool Win
+	{
+		get { return win; }
+		set { win = value; }
+	}
+	
+	public bool End
+	{
+		get { return end; }
+		set { end = value; }
+	}
+	
+	public bool Begin
+	{
+		get { return begin; }
+		set { begin = value; }
+	}
+
+	public float Timer
+	{
+		get { return timer; }
+		set { timer = value; }
+	}
+}
