45416a3cbe2f0cbb1f6a179633d3c4f141f4bc76
Marine Charra <marine.charra@github.com>
2018-06-08 11:58:01 +0200

    Projet
    bloc rotate dans maze

---
+++ Projet/Assets/Menu/settingsMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class settingsMenu : MonoBehaviour
+{
+
+	public Slider volume;
+	public Slider volumee;
+
+	public AudioSource myMusic;
+
+	private void Start()
+	{
+		volume = volume.GetComponent<Slider>();
+
+		volumee = volumee.GetComponent<Slider>();
+
+		myMusic = myMusic.GetComponent<AudioSource>();
+	}
+
+	void Update()
+	{
+
+		myMusic.volume = volume.value;
+		volumee.value = volume.value;
+		
+
+	}
+}
